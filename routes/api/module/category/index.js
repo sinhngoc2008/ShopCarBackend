bc49aaa817c4f1bec71a5d3f9e04c970e47f51e1
Thang Din Lee <thangld2407@gmail.com>
2023-05-11 11:49:25 +0700

    add detail category

--- routes/api/module/category/index.js
+++ routes/api/module/category/index.js
@@ -3,9 +3,11 @@ const listCategory = require('../../../../controller/Category/ListCategoryContro
 const createCategory = require('../../../../controller/Category/CreateCategoryController');
 const DeleteCategory = require('../../../../controller/Category/DeleteCategory');
 const editCategory = require('../../../../controller/Category/UpdateCategoryController');
+const { default: DetailCategory } = require('../../../../controller/Category/DetailCategory');
 routerCategory.get('/list', listCategory);
 routerCategory.post('/create', createCategory);
 routerCategory.post('/delete', DeleteCategory);
 routerCategory.post('/edit', editCategory);
+routerCategory.post('/detail', DetailCategory);
 
 module.exports = routerCategory;
